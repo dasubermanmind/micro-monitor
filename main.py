bfa9c96a8bec795cc1e1b039692beb58c0c42262
dasubermanmind <ero33@drexel.edu>
2022-10-16 09:43:00 -0400

    First pass

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# TODO: Be able to monitor a directory and when a file gets added, we do something. [ ]
+
+# TODO: Research actions: 1) Email 2) Push notifications (mobile) 3) Upload to a server (Django/FastAPI) [  ]
+
+
+
+
+
+
+
+
+
+
+if __name__ == 'main':
+    print('HELLO WORLD')
+
